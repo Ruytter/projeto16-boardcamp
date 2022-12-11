6db1d46454676ae3c202c67bfea7cc68dd5e6e3e
Ruytter <ruytter.benjamin@gmail.com>
2022-12-10 22:05:26 -0300

    feat: GET/games, GET/games/:name e POST/games

--- src/routes/boardcamp.routes.js
+++ src/routes/boardcamp.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 
-import {getCategories, insertCategory} from '../controllers/boardcamp.controller.js'
+import {getCategories, insertCategory, getGames, getGameByName, insertGame} from '../controllers/boardcamp.controller.js'
 
 const router = Router();
 
 router.get("/categories", getCategories);
 router.post("/categories", insertCategory);
+router.get("/games", getGames);
+router.get("/games/:name", getGameByName);
+router.post("/games", insertGame);
 
 export default router;
