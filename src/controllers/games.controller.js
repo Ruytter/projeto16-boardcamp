8d61d4bb452c62bc04bd11c4e5f0467082628232
Ruytter <ruytter.benjamin@gmail.com>
2022-12-12 10:28:14 -0300

    fixe: games.controllers.js

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -51,7 +51,7 @@ export async function insertGame(req, res) {
     }
 
     await connection.query(
-      "INSERT INTO games (name, image, stockTotal, categoryId, pricePerDay) VALUES ($1, $2, $3, $4, $5);",
+      'INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5);',
       [name, image, stockTotal, categoryId, pricePerDay]
     );
     res.sendStatus(201);
